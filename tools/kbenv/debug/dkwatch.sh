576a9d75da407b4e8aa5d0e12d74a0ba9c61c174
taosy <shaoyu_tao@126.com>
2020-01-03 01:41:50 -0500

    Add script base on inotifywait to watch fs

---
+++ tools/kbenv/debug/dkwatch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function watch()
+{
+    fn=$1
+    if [ ! -e $fn ]
+    then
+        echo "file $fn not exist"
+        exit 1
+    fi
+    flist=$(<$fn)
+    reg='('
+    for i in $flist;do
+         reg=${reg}${i}'|'
+    done
+    reg=${reg:0:(-1)}')'
+    echo -n ${reg} | xargs inotifywait -m  --timefmt '%Y-%m-%d %H:%M'  --format '%T:[%e] %w%f' /etc  /var --exclude
+
+}
+
+while getopts "e:" arg                       
+do
+        case $arg in
+             e)
+                watch $OPTARG 
+                ;;
+        esac
+done
